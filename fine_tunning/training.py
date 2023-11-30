bbed5b833a2d690c38e9fc1a02360643b1566706
Marco Navarro G <mjosng24@gmail.com>
2023-11-29 21:12:01 -0600

    changes

--- fine_tunning/training.py
+++ fine_tunning/training.py
@@ -1,10 +1,9 @@
 import replicate
 
 training = replicate.trainings.create(
-  version="meta/llama-2-7b:73001d654114dad81ec65da3b834e2f691af1e1526453189b7bf36fb3f32d0f9",
+  version="meta/llama-2-13b-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5",
   input={
-    "train_data": "https://raw.githubusercontent.com/majonaga/chatbot-openllama2-python/main/fine_tunning/data-test.jsonl",
-    "num_train_epochs": 3
+    "train_data": "https://raw.githubusercontent.com/majonaga/chatbot-openllama2-python/main/fine_tunning/data.jsonl",
   },
   destination="majonaga/llama2-refugio-test"
 )
